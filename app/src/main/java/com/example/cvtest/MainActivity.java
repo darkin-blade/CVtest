a3e602ea9ff17ba91876b09b2292187f25a392e5
lynx <2104934038@qq.com>
2019-10-23 10:10:27 +0800

    todo:使用openCV处理图片

--- app/src/main/java/com/example/cvtest/MainActivity.java
+++ app/src/main/java/com/example/cvtest/MainActivity.java
@@ -3,18 +3,29 @@ package com.example.cvtest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
     static {
         if (!OpenCVLoader.initDebug()) {
-            Log.d("openCV", "init failed");
+            infoLog("openCV init failed");
         }
     }
+
+    static public String appPath = null;
+    static public String img1Path = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
         if (check_result != PackageManager.PERMISSION_GRANTED) {// 没有`写`权限
             ActivityCompat.requestPermissions(this, new String[]{permission}, 1);// 获取`写`权限
         }
+
+        // 初始化路径字符串
+        appPath = getExternalFilesDir("").getAbsolutePath();
+        img1Path = appPath + "/" + "img_1.png";
+        infoToast(this, appPath);
+
+        // 初始化图片
+        ImageView img_1 = findViewById(R.id.img_1);
+        Bitmap tempImg = BitmapFactory.decodeFile(img1Path);// 打开本机图片
+        img_1.setImageBitmap(tempImg);
+    }
+
+    static public void infoLog(String log) {
+        Log.i("fuck", log);
+    }
+
+    static public void infoToast(Context context, String log) {
+        Toast toast =  Toast.makeText(context, log, Toast.LENGTH_SHORT);
+        View view = toast.getView();
+        TextView textView = view.findViewById(android.R.id.message);
+        textView.setTextColor(Color.rgb(0x00, 0x00, 0x00));
+        toast.show();
     }
 }
