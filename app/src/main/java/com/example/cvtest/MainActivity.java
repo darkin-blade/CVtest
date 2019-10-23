2c559f5c729bf659768526160bcc81dfe30bf3a6
lynx <1197225628@qq.com>
2019-10-23 09:36:03 +0800

    first

---
+++ app/src/main/java/com/example/cvtest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.cvtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("openCV", "init failed");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init() {
+        ;
+    }
+}
