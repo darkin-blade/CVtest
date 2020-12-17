93e452deb2925a66fd192a78c56c10a16cacec05
lynx <2104934038@qq.com>
2020-12-17 16:43:52 +0800

    temp: 霍夫变换

---
+++ houghLinesOfficial.cpp
@@ -0,0 +1,106 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+#define LOG(format, ...) \
+  printf("\033[1;32m[%s, %d]" format "\33[0m\n", __func__, __LINE__, ## __VA_ARGS__)
+
+void show_img(const char *window_name, Mat img) {
+  namedWindow(window_name, WINDOW_NORMAL);
+  resizeWindow(window_name, 1280, 720);
+  imshow(window_name, img);
+  waitKey(0);
+
+  // 保存图片
+  char img_name[128];
+  int savable = 0;
+  for (int i = 0; i < 100; i ++) {
+    sprintf(img_name, "../result_%d.png", i);
+    if (fopen(img_name, "r") == NULL) {
+      savable = 1;
+      break;
+    }
+  }
+  if (savable) {
+    imwrite(img_name, img);
+  } else {
+    LOG("can't save img");
+  }
+}
+
+int main(int argc, char** argv)
+{
+    // Declare the output variables
+    Mat dst, cdst, cdstP;
+    const char* default_file = "../2.jpg";
+    const char* filename = argc >=2 ? argv[1] : default_file;
+    // Loads an image
+    Mat src = imread( filename, IMREAD_GRAYSCALE );
+    // Check if image is loaded fine
+    if(src.empty()){
+        printf(" Error opening image\n");
+        printf(" Program Arguments: [image_name -- default %s] \n", default_file);
+        return -1;
+    }
+    // Edge detection
+    Canny(
+      src, 
+      dst, 
+      50,  // 50
+      200, // 200
+      3
+    );
+    // Copy edges to the images that will display the results in BGR
+    cvtColor(dst, cdst, COLOR_GRAY2BGR);
+    cdstP = cdst.clone();
+    // Standard Hough Line Transform
+    vector<Vec2f> lines; // will hold the results of the detection
+    HoughLines(
+      dst, 
+      lines, 
+      1, 
+      CV_PI/180, 
+      150, // 150
+      0, // 0 
+      0  // 0
+    ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        line( cdst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
+    }
+    // Probabilistic Line Transform
+    vector<Vec4i> linesP; // will hold the results of the detection
+    HoughLinesP(
+      dst, 
+      linesP,
+      1, 
+      CV_PI/180, 
+      50, // hough变换图像空间值最大点 50
+      200, // 最小值线长度 50
+      20   // 直线间隙最大值 10
+    ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < linesP.size(); i++ )
+    {
+        Vec4i l = linesP[i];
+        line( cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+    }
+    // Show results
+    // show_img("Source", src);
+    // show_img("Detected Lines (in red) - Standard Hough Line Transform", cdst);
+    show_img("Detected Lines (in red) - Probabilistic Line Transform", cdstP);
+    // Wait and Exit
+    return 0;
+}
